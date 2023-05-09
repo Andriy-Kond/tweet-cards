6705e90a4da3ef13fdab8acf2401604b06fb86b3
Andrii <107649409+Andriy-Kond@users.noreply.github.com>
2023-05-09 14:15:18 +0300

    fix: lint warnings

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,6 +1,4 @@
-import { Link } from 'react-router-dom';
 import css from './HomePage.module.css';
-import doSomeGreat from '../../assets/do-some-great.jpg';
 
 const HomePage = () => {
   return <section className={css.hero}></section>;
