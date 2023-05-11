65d7e8687e48f98011b3850687e27292c918d3f6
Andrii <107649409+Andriy-Kond@users.noreply.github.com>
2023-05-11 21:16:16 +0300

    upd: pagination

--- src/components/TweetCards/TweetCards.jsx
+++ src/components/TweetCards/TweetCards.jsx
@@ -1,18 +1,27 @@
 import { RenderTweets } from 'components/RenderTweets/RenderTweets';
 import { PaginationList } from 'Layout/PaginationList/PaginationList';
 import { useSelector } from 'react-redux';
-import { selectCurrentPage, selectFilteredTweets } from 'redux/selectors';
+import {
+  selectCurrentPage,
+  selectDownloadedTweets,
+  selectFilteredTweets,
+} from 'redux/selectors';
 import { CARDS_PER_PAGE } from 'Services/variables';
 
 export const TweetCards = () => {
-  const filteredTweets = useSelector(selectFilteredTweets);
-  const currentPage = useSelector(selectCurrentPage);
-  const lastIndex = currentPage * CARDS_PER_PAGE;
-  const currentTweets = filteredTweets?.slice(0, lastIndex);
+  // const currentPage = useSelector(selectCurrentPage);
+  // const lastIndex = currentPage * CARDS_PER_PAGE;
+  // const filteredTweets = useSelector(selectFilteredTweets);
+  // const currentTweets = filteredTweets?.slice(0, lastIndex);
+
+  const downloadedTweets = useSelector(selectDownloadedTweets);
 
   return (
     <>
-      {currentTweets?.length > 0 && <RenderTweets tweets={currentTweets} />}
+      {/* {currentTweets?.length > 0 && ( */}
+
+      <RenderTweets currentTweets={downloadedTweets} />
+      {/* )} */}
       <PaginationList />
     </>
   );
--- src/pages/Tweets/TweetsPage.jsx
+++ src/pages/Tweets/TweetsPage.jsx
@@ -1,14 +1,21 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { useGetUsersQuery } from 'redux/tweetsApi';
 import {
-  setAllTweets,
+  setCurrentPage,
+  setDownloadedTweets,
   setFilteredTweets,
+  setIsLoading,
   setTotalPages,
   setUsersFilter,
 } from 'redux/sliceUsers';
 import { PreLoader } from 'Layout/Preloader/PreLoader';
 import { TweetCards } from 'components/TweetCards/TweetCards';
-import { selectFilteredTweets, selectUsersFilter } from 'redux/selectors';
+import {
+  selectFilteredTweets,
+  selectFilter,
+  selectCurrentPage,
+  selectDownloadedTweets,
+} from 'redux/selectors';
 import { CARDS_PER_PAGE } from 'Services/variables';
 import { useEffect } from 'react';
 import { MyDropdown } from 'components/Dropdown/Dropdown';
@@ -17,22 +24,38 @@ import css from './TweetsPage.module.css';
 import { Link } from 'react-router-dom';
 
 const TweetsPage = () => {
-  const dataQuery = useGetUsersQuery();
-  const { data: allTweets, isLoading, isError, error } = dataQuery;
   const dispatch = useDispatch();
-  const userFilter = useSelector(selectUsersFilter);
+  const currentPage = useSelector(selectCurrentPage);
+  const {
+    data: limitedTweets,
+    isLoading,
+    isError,
+    error,
+  } = useGetUsersQuery(currentPage);
+  // const { data: limitedTweets, isLoading, isError, error } = getQuery;
+
+  const downloadedTweets = useSelector(selectDownloadedTweets);
+  const userFilter = useSelector(selectFilter);
   const filteredTweets = useSelector(selectFilteredTweets);
-  const totalPages =
-    filteredTweets?.length > 0
-      ? Math.ceil(filteredTweets?.length / CARDS_PER_PAGE)
-      : 1;
+
+  // const totalPages =
+  //   filteredTweets?.length > 0
+  //     ? Math.ceil(filteredTweets?.length / CARDS_PER_PAGE)
+  //     : 1;
+
+  // dispatch(setIsLoading(isLoading));
 
   useEffect(() => {
-    dispatch(setAllTweets(allTweets));
-    allTweets?.length > 0 && dispatch(setFilteredTweets());
-    dispatch(setUsersFilter(userFilter));
-    dispatch(setTotalPages(totalPages));
-  }, [allTweets, dispatch, isError, isLoading, totalPages, userFilter]);
+    if (limitedTweets?.length > 0) {
+      dispatch(setDownloadedTweets(limitedTweets));
+    }
+  }, [dispatch, limitedTweets]);
+
+  // useEffect(() => {
+  //   allTweets?.length > 0 && dispatch(setFilteredTweets(allTweets));
+  //   dispatch(setUsersFilter(userFilter));
+  //   dispatch(setTotalPages(totalPages));
+  // }, [allTweets, dispatch, totalPages, userFilter]);
 
   return (
     <div>
@@ -43,8 +66,9 @@ const TweetsPage = () => {
       <div className={css.myDropdown}>
         <MyDropdown />
       </div>
+
       {isError && <Error error={error} />}
-      {isLoading ? <PreLoader isLoading={isLoading} /> : <TweetCards />}
+      {isLoading ? <PreLoader /> : <TweetCards />}
     </div>
   );
 };
